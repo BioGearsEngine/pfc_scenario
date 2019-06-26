b1a58abfeb0398667c63faa41cea65d4afa89915
Steven A White <sawhite@ara.com>
2019-06-26 14:19:33 -0400

    Introducing a Splash Screen Mechanic

--- projects/scenario_builder/cpp/main.cpp
+++ projects/scenario_builder/cpp/main.cpp
@@ -11,35 +11,40 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 **************************************************************************************/
 
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickStyle>
+#include <QSplashScreen>
 
-#include "Scenario.h"
+#include "Actors.h"
 #include "Locations.h"
 #include "Objects.h"
+#include "Scenario.h"
 #include "Scenes.h"
-#include "Actors.h"
 
 int main(int argc, char* argv[])
 {
   /* Commenti
   g the following line resolves the issue but then UI elements looks small */
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-  QGuiApplication app(argc, argv);
+  QApplication app(argc, argv);
   QQuickStyle::setStyle("Material");
   QQmlApplicationEngine engine;
 
-  qmlRegisterType<pfc::Scenario>("com.ara.pfc.ScenarioModel", 1,0, "ScenarioModel");
-  qmlRegisterType<pfc::LocationSequence>("com.ara.pfc.ScenarioModel.Locations", 1,0, "Locations");
-  qmlRegisterType<pfc::ObjectSequence>("com.ara.pfc.ScenarioModel.Objects"  , 1,0, "Objects");
-  qmlRegisterType<pfc::ActorSequence>("com.ara.pfc.ScenarioModel.Actors"   , 1,0, "Actors");
-  qmlRegisterType<pfc::NarativeSequence>("com.ara.pfc.ScenarioModel.Narative"   , 1,0, "Naratives");
+  qmlRegisterType<pfc::Scenario>("com.ara.pfc.ScenarioModel", 1, 0, "ScenarioModel");
+  qmlRegisterType<pfc::LocationSequence>("com.ara.pfc.ScenarioModel.Locations", 1, 0, "Locations");
+  qmlRegisterType<pfc::ObjectSequence>("com.ara.pfc.ScenarioModel.Objects", 1, 0, "Objects");
+  qmlRegisterType<pfc::ActorSequence>("com.ara.pfc.ScenarioModel.Actors", 1, 0, "Actors");
+  qmlRegisterType<pfc::NarativeSequence>("com.ara.pfc.ScenarioModel.Narative", 1, 0, "Naratives");
 
+
+  QPixmap pixmap("D:/remotes/sed-stash/biogears/pfc/scenario/build-msvc/img/sustain_splash.png");
+  qDebug() << QString("%1,%2").arg(pixmap.height()).arg(pixmap.width());
+  QSplashScreen splash(pixmap);
+  splash.show();
   engine.load(QUrl(QLatin1String("qrc:/Main.qml")));
   if (engine.rootObjects().isEmpty())
     return -1;
-
+  splash.close();
   return app.exec();
 }
-       
